a3445e01792b9862b3309b1fef92ba2f3e9db77a
Marcus Gajozo <marcusgajozo@gmail.com>
2026-03-03 14:36:14 -0400

    refactor: use i18n messsage

--- src/modules/authentication/dtos/sign-in.dto.ts
+++ src/modules/authentication/dtos/sign-in.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class SignInDto {
-  @IsEmail()
+  @IsEmail({}, { message: i18nValidationMessage('validation.INVALID_EMAIL') })
   email: string;
 
   @IsString()
+  @IsNotEmpty({ message: i18nValidationMessage('validation.NOT_EMPTY') })
   password: string;
 }
