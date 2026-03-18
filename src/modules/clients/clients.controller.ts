4c4babcb72cb981095da700841af65d3f9e0c19d
Marcus Gajozo <marcusgajozo@gmail.com>
2026-03-18 17:36:48 -0400

    refactor(clients): implementa CRUD de clientes

--- src/modules/clients/clients.controller.ts
+++ src/modules/clients/clients.controller.ts
@@ -3,13 +3,15 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
   Query,
 } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { User } from 'src/common/decorator/user.decorator';
+import { UserAuth } from 'src/common/decorator/user-auth.decorator';
 import { PaginationQueryDto } from 'src/common/dtos/pagination.dto';
 import { VerifyUUIDIdPipe } from 'src/common/pipes/verify-uuid-id.pipe';
 import { ClientsService } from './clients.service';
@@ -22,43 +24,48 @@ export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async create(
     @Body() createClientDto: CreateClientDto,
-    @User('userId') userId: string,
+    @UserAuth('userId') userId: string,
   ) {
-    return;
+    return await this.clientsService.create(createClientDto, userId);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   async findAll(
     @Query() paginationQueryDto: PaginationQueryDto,
-    @User('userId') userId: string,
+    @UserAuth('userId') userId: string,
   ) {
-    return;
+    return await this.clientsService.findAll(paginationQueryDto, userId);
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   async findOne(
     @Param('id', VerifyUUIDIdPipe) id: string,
-    @User('userId') userId: string,
+    @UserAuth('userId') userId: string,
   ) {
-    return;
+    return await this.clientsService.findOne(id, userId);
   }
 
   @Patch(':id')
+  @HttpCode(HttpStatus.OK)
   async update(
     @Param('id', VerifyUUIDIdPipe) id: string,
     @Body() updateClientDto: UpdateClientDto,
-    @User('userId') userId: string,
+    @UserAuth('userId') userId: string,
   ) {
-    return;
+    return await this.clientsService.update(id, updateClientDto, userId);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async remove(
     @Param('id', VerifyUUIDIdPipe) id: string,
-    @User('userId') userId: string,
+    @UserAuth('userId') userId: string,
   ) {
-    return;
+    return await this.clientsService.remove(id, userId);
   }
 }
--- src/modules/clients/clients.service.ts
+++ src/modules/clients/clients.service.ts
@@ -1,35 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dtos/pagination.dto';
 import { Repository } from 'typeorm';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { ClientEntity } from './entities/client.entity';
+import { i18nValidationMessage } from 'src/common/helper/i18n-validation-message';
 
 @Injectable()
 export class ClientsService {
   constructor(
     @InjectRepository(ClientEntity)
-    private readonly procedureRepository: Repository<ClientEntity>,
+    private readonly clientRepository: Repository<ClientEntity>,
   ) {}
 
   async create(createClientDto: CreateClientDto, userId: string) {
-    return;
+    const { name, phone } = createClientDto;
+
+    const client = this.clientRepository.create({
+      name,
+      phone,
+      user: { id: userId },
+    });
+
+    return await this.clientRepository.save(client);
   }
 
   async findAll(paginationQueryDto: PaginationQueryDto, userId: string) {
-    return;
+    const { limit, page } = paginationQueryDto;
+
+    const [clients, totalCount] = await this.clientRepository.findAndCount({
+      where: { userId },
+      skip: page - 1,
+      take: limit,
+    });
+
+    return { clients, totalCount };
   }
 
   async findOne(id: string, userId: string) {
-    return;
+    const client = await this.clientRepository.findOne({
+      where: { id, userId },
+    });
+
+    return client;
   }
 
   async update(id: string, updateClientDto: UpdateClientDto, userId: string) {
-    return;
+    const client = await this.findOne(id, userId);
+
+    if (!client) {
+      throw new NotFoundException(
+        i18nValidationMessage('validation.NOT_FOUND'),
+      );
+    }
+
+    const { name, phone } = updateClientDto;
+    const updatedAt = new Date();
+
+    return await this.clientRepository.update(client.id, {
+      name,
+      phone,
+      updatedAt,
+    });
   }
 
   async remove(id: string, userId: string) {
-    return;
+    const client = await this.clientRepository.findOne({
+      where: { id, userId },
+    });
+
+    if (!client) {
+      return null;
+    }
+
+    return await this.clientRepository.remove(client);
   }
 }
