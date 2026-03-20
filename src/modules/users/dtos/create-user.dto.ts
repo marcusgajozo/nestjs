9a4c57b5db99364a3245c1b63620cf2b77cabd1a
Marcus Gajozo <marcusgajozo@gmail.com>
2026-03-20 15:31:52 -0400

    refactor(users): atualiza importação de i18n-validation-message

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,25 +1,20 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { i18nValidationMessage } from 'nestjs-i18n';
-import { I18nTranslations } from 'src/generated/i18n.generated';
+import { i18nValidationMessage } from 'src/common/helper/i18n-validation-message';
 
 export class CreateUserDto {
   @IsString({
-    message: i18nValidationMessage<I18nTranslations>(
-      'validation.INVALID_STRING',
-    ),
+    message: i18nValidationMessage('validation.INVALID_STRING'),
   })
   @IsNotEmpty({
-    message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
+    message: i18nValidationMessage('validation.NOT_EMPTY'),
   })
   name: string;
 
   @IsString({
-    message: i18nValidationMessage<I18nTranslations>(
-      'validation.INVALID_STRING',
-    ),
+    message: i18nValidationMessage('validation.INVALID_STRING'),
   })
   @IsNotEmpty({
-    message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
+    message: i18nValidationMessage('validation.NOT_EMPTY'),
   })
   phone: string;
 }
