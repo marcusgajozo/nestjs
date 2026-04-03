34fc62a68921591f60ebe48a6afcd87cc5c9d883
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-03 11:56:36 -0400

    feat(appointments): add status and client notification enums

---
+++ src/modules/appointments/enums/notify-client.enum.ts
@@ -0,0 +1,5 @@
+export enum NotifyReturnClientEnum {
+  NOT_NOTIFY = 'not_notify',
+  NOTIFIED = 'notified',
+  PENDING_NOTIFICATION = 'pending_notification',
+}
---
+++ src/modules/appointments/enums/status-appointment.enum.ts
@@ -0,0 +1,6 @@
+export enum StatusAppointmentEnum {
+  PENDING_STATUS = 'pending_status',
+  SCHEDULED = 'scheduled',
+  CANCELED = 'canceled',
+  DONE = 'done',
+}
